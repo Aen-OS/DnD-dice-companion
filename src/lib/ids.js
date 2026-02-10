5a26809158ae85dca59a2de44cbdb7eb97c908c6
anedjai <46544513+Aen-OS@users.noreply.github.com>
2026-02-10 19:17:20 +0000

    Revert "fixed build issue"
    This reverts commit e4ce2db1ab5fd77a1b849e3e05980b22effd175b.

--- src/lib/ids.js
+++
@@ -1,9 +0,0 @@
-export function makeId() {
-  if (typeof crypto !== "undefined" && crypto.randomUUID)
-    return crypto.randomUUID();
-  return String(Date.now()) + "-" + String(Math.random()).slice(2);
-}
-
-export function nowStamp() {
-  return new Date().toLocaleString();
-}
